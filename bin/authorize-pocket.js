0e435a57be8a82d922698383e915768d5a111ffd
vicchi <gary@vicchi.org>
2015-01-29 12:58:38 +0000

    Move support scripts from utils to bin

--- bin/authorize-pocket.js
+++ bin/authorize-pocket.js
@@ -1,7 +1,7 @@
 var GetPocket = require('node-getpocket');
 var Express = require('express');
 var StdIo = require('stdio');
-var SQLite3 = require('sqlite3');
+// var SQLite3 = require('sqlite3');
 
 var options = StdIo.getopt({
     'consumerkey': {
@@ -93,41 +93,41 @@ function access_token_handler(error, response, body) {
         config.user_name = json.username;
         console.log('Received access token: ' + config.access_token + ' for user ' + config.user_name);
         app.locals.res.send('<p>' + 'Received access token: ' + config.access_token + ' for user ' + config.user_name + '</p>');
-        secure_credentials();
+        // secure_credentials();
     }
 }
 
-function secure_credentials() {
-    var db = new SQLite3.Database('auth.sqlite3');
-
-    db.serialize(function() {
-        var sql = 'DROP TABLE IF EXISTS pocket_auth';
-        db.run(sql, function(err) {
-            if (err) {
-                console.log('"' + sql + '" failed: ' + err);
-            }
-        });
-
-        sql = 'CREATE TABLE IF NOT EXISTS pocket_auth (id INTEGER PRIMARY KEY, consumer_key TEXT, access_token TEXT, user_name TEXT);';
-        db.run(sql, function(err) {
-            if (err) {
-                console.log('"' + sql + '" failed: ' + err);
-            }
-        });
-
-        sql = 'INSERT INTO pocket_auth (id, consumer_key, access_token, user_name) VALUES($id,$consumer_key,$access_token,$user_name)';
-        var params = {
-            $id: 1,
-            $consumer_key: config.consumer_key,
-            $access_token: config.access_token,
-            $user_name: config.user_name
-        };
-        db.run(sql, params, function(err) {
-            if (err) {
-                console.log('"' + sql + '" failed: ' + err);
-            }
-        });
-    });
-
-    db.close();
-}
+// function secure_credentials() {
+//     var db = new SQLite3.Database('auth.sqlite3');
+//
+//     db.serialize(function() {
+//         var sql = 'DROP TABLE IF EXISTS pocket_auth';
+//         db.run(sql, function(err) {
+//             if (err) {
+//                 console.log('"' + sql + '" failed: ' + err);
+//             }
+//         });
+//
+//         sql = 'CREATE TABLE IF NOT EXISTS pocket_auth (id INTEGER PRIMARY KEY, consumer_key TEXT, access_token TEXT, user_name TEXT);';
+//         db.run(sql, function(err) {
+//             if (err) {
+//                 console.log('"' + sql + '" failed: ' + err);
+//             }
+//         });
+//
+//         sql = 'INSERT INTO pocket_auth (id, consumer_key, access_token, user_name) VALUES($id,$consumer_key,$access_token,$user_name)';
+//         var params = {
+//             $id: 1,
+//             $consumer_key: config.consumer_key,
+//             $access_token: config.access_token,
+//             $user_name: config.user_name
+//         };
+//         db.run(sql, params, function(err) {
+//             if (err) {
+//                 console.log('"' + sql + '" failed: ' + err);
+//             }
+//         });
+//     });
+//
+//     db.close();
+// }
